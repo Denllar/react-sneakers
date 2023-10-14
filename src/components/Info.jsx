2bf53e19cb3469c0ca4102d36a2a56592466067b
Denllar <denisgabbasov06.09.2004@gmail.com>
2023-10-14 17:20:57 +0500

    fix5

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,7 +9,7 @@ const Info = ({ title, image,  description, buttonDescription }) => {
             <img className='mb-20' src={image} alt='empty-box' />
             <h2>{title}</h2>
             <p className='opacity-6'>{description}</p>
-            <Link to="/react-sneakers">
+            <Link to="/react-sneakers/">
                 <button onClick={()=>setOpenCard(false)} className='greenButton mb-50 pr-35'>
                     <img className='mr-20' src='img/arrow.svg' alt='arrow' />
                     {buttonDescription}
