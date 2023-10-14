eb66790b81184a6862c3eb231e31951db4de5aab
Denllar <denisgabbasov06.09.2004@gmail.com>
2023-10-14 17:16:28 +0500

    fix3

--- src/App.js
+++ src/App.js
@@ -175,7 +175,7 @@ function App() {
             }
           ></Route>
 
-          <Route path='favorites' exact element={
+          <Route path='/react-sneakers/favorites' exact element={
             <Favorites
               onAddToCart={onAddToCart}
               onAddToFavorites={onAddToFavorites}
@@ -183,7 +183,7 @@ function App() {
           }
           ></Route>
 
-          <Route path='orders' exact element={
+          <Route path='/react-sneakers/orders' exact element={
             <Orders
             />
           }
